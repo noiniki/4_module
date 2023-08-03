ba11d842149a472ce9aeba60f112cc95df194d31
Mary <marynik8822@gmail.com>
2023-08-03 16:32:48 +0700

    седьмой урок

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(" Заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text= 'отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f"id = {self.id},title = {self.title}, price = {self.price}"
