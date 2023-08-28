9d0a028a1ab3d55cdc388c50af2cede3c0198cd7
Mary <marynik8822@gmail.com>
2023-08-28 16:09:33 +0700

    правильное дз 10

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Advertisement(models.Model):
+class Advertisement (models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
